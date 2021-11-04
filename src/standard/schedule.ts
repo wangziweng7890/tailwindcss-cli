fca9fa34fc71d501b8012917e96234cfb26d98bb
wangziweng7890 <865302637@qq.com>
2021-11-04 14:50:09 +0800

    [add]修改交互

--- src/standard/schedule.ts
+++ src/standard/schedule.ts
@@ -7,40 +7,37 @@ import ora from 'ora';
 export default class Schedule {
   
   public run() {
+    const spinner = ora(chalk.blue('正在生成tailwindcss文件，请稍等...')).start();
     this.initTailwindCss();
+    spinner.text = '正在生成tailwind.config.js文件，请稍等...';
     this.initTailwindConfig();
+    spinner.text = '正在生成postcss.config.js文件，请稍等...';
     this.initPostcssConfig();
+    spinner.text = '配置文件更新完成，正在安装相关依赖，请耐心等待...';
     this.installDependencies(); // 安装相关依赖
+    spinner.stop();
   }
 
   private initTailwindCss() {
-    const spinner = ora(chalk.blue('正在生成tailwindcss文件，请稍等...')).start();
     const file = fs.readFileSync(worker.getTemplateDir('tailwind.css'));
     const filePath = path.join(worker.getWorkDir(), './tailwind.css');
     fs.writeFileSync(filePath, file);
-    spinner.stop();
   }
 
   private initPostcssConfig() {
-    const spinner = ora(chalk.blue('正在生成postcss.config.js文件，请稍等...')).start();
     const file = fs.readFileSync(worker.getTemplateDir('postcss.config.js'));
     const filePath = path.join(worker.getWorkDir(), './postcss.config.js');
     fs.writeFileSync(filePath, file);
-    spinner.stop();
   }
 
   private initTailwindConfig() {
-    const spinner = ora(chalk.blue('正在生成tailwind.config.js文件，请稍等...')).start();
     const filePath = path.join(worker.getWorkDir(), 'tailwind.config.js');
     const file = fs.readFileSync(worker.getTemplateDir('./tailwind.config.js'));
     fs.writeFileSync(filePath, file);
-    spinner.stop();
   }
 
   private installDependencies() {
-    const spinner = ora(chalk.blue('配置文件更新完成，正在安装相关依赖，请耐心等待...')).start();
     const reply = shell.exec('cnpm install tailwindcss@1.9.6 postcss-preset-env@6.7.0 -D');
-    spinner.stop();
     if (reply.code !== 0) {
       shell.exit(1);
     }
